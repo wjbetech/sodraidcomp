61f0d691218ab3f1d56d509bec4af6d99485aa90
wjbetech <wjbetech@gmail.com>
2024-06-18 01:40:14 +0900

    Fixed JSON more

--- src/utils/specDataHandler.ts
+++ src/utils/specDataHandler.ts
@@ -1,24 +1,10 @@
 import classesAndSpecs from '../specs.json';
 
-// build object for each class
-const allClassDetails: { class: string; specs: [] }[] = [];
-
-// build class + spec objects
-const classWithSpecs: { class: string; specs: string }[] = [];
-
-for (const classObject of classesAndSpecs) {
-  for (const [className, specs] of Object.entries(classObject)) {
-    const specNames = specs.map((specObj: object) => Object.keys(specObj));
-    classWithSpecs.push({
-      class: className,
-      specs: specNames
-    });
-  }
-}
-
 // grab class names
-const classNames = classesAndSpecs.map((classes) => {
-  return Object.keys(classes)[0];
+const classAndSpecData = classesAndSpecs;
+
+classAndSpecData.forEach((className, idx) => {
+  return className[idx];
 });
 
-export default { classNames, classWithSpecs, allClassDetails };
+export default { classAndSpecData };
