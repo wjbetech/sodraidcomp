a82ae80e341719952e2ffece3657cf903bd5d49f
William East <wjbetech@gmail.com>
2024-07-08 17:30:53 +0900

    Added some typing in the dataHandler util

--- src/utils/specDataHandler.ts
+++ src/utils/specDataHandler.ts
@@ -1,21 +1,36 @@
 import myData from "../specs.json";
 
+interface Spec {
+	specName: string;
+}
+
+interface WowClass {
+	className: string;
+	specs: Spec[];
+}
+
+interface ClassAndSpecs {
+	className: string;
+	specName: string;
+}
+
+// ensure all future uses of data are validated
+const validatedData: WowClass[] = myData;
+
 // grab *only* class names
-const classNames = myData.map((d) => d.className);
+const classNames: string[] = validatedData.map((d) => d.className);
 
 // grab *all* data raw
-const allData = myData;
+const allData: WowClass[] = validatedData;
 
 // grab classes with their specs & info
-const classAndSpecs = [];
+const classAndSpecs: ClassAndSpecs[] = [];
 
-for (const wowClass of myData) {
+for (const wowClass of validatedData) {
 	const { className, specs } = wowClass;
-	if (!classAndSpecs[className]) {
-		classAndSpecs[className] = [];
+	for (const spec of specs) {
+		classAndSpecs.push({ className, specName: spec.specName });
 	}
-
-	classAndSpecs[className].push(specs);
 }
 
 export { classNames, allData, classAndSpecs };
