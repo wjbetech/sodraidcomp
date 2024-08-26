e7e5145095da0c5cd40439b81c100ca1c7cf1d43
William East <wjbetech@gmail.com>
2024-08-26 14:40:31 +0900

    Work on implementing Swapy drag n drop

--- src/components/PlayerSlot.tsx
+++ src/components/PlayerSlot.tsx
@@ -2,7 +2,6 @@ export default function PlayerSlot() {
 	return (
 		<div className="flex items-center justify-center my-2 border-2 border-gray-500 rounded-md h-[34px] w-full hover:bg-white/10">
 			{" "}
-			-{" "}
 		</div>
 	);
 }
