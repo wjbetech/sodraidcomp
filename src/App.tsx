bc1a37a796df553a6ae56f5ad705cec78f617ad7
wjbetech <wjbetech@gmail.com>
2024-08-26 16:52:29 +0900

    Deleted useless assets folder, fixed DndContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import "./App.css";
-import WoWClasses from "./components/ClassesAndSpecs";
-import NavBar from "./components/NavBar";
-import RaidGrid from "./components/RaidGrid";
-import { DndContext } from "@dnd-kit/core";
+import './App.css';
+import WoWClasses from './components/ClassesAndSpecs';
+import NavBar from './components/NavBar';
+import RaidGrid from './components/RaidGrid';
+import { DndContext, DragStartEvent } from '@dnd-kit/core';
 
 function App() {
-	return (
-		<DndContext>
-			<NavBar />
-			<main className="w-[80%] m-auto">
-				<h1 className="">SoD Raid Composition</h1>
-				<span className="text-xs text-gray-400">
-					by Valruna @ Living Flame EU
-				</span>
-				<WoWClasses />
-				<div className="m-auto w-full">
-					<RaidGrid />
-				</div>
-			</main>
-		</DndContext>
-	);
+  const handleDragStart = (event: DragStartEvent) => {
+    console.log('Dragging started!');
+    console.log(event);
+  };
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    console.log('Dragging ended!');
+    console.log(event);
+  };
+
+  return (
+    <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+      <NavBar />
+      <main className="w-[80%] m-auto">
+        <h1 className="">SoD Raid Composition</h1>
+        <span className="text-xs text-gray-400">by Valruna @ Living Flame EU</span>
+        <WoWClasses />
+        <div className="m-auto w-full">
+          <RaidGrid />
+        </div>
+      </main>
+    </DndContext>
+  );
 }
 
 export default App;
